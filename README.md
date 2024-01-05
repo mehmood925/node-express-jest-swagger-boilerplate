fef97bf74370857e96081424a4de2420f979796e
mehmoodtkxel <mehmood.hussain@tkxel.com>
2024-01-05 18:39:49 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This boilerplate comes with express js framework and it uses jest for unit testing.
+Follow the steps to run the boilerplate. Make sure to create .env file before you run the app.
+
+execute the following commands in order
+
+> npm install
+> npm run migrate
+> npm test
+> npm start
+
+To see the documentation, please visit the following link
+https://docs.google.com/document/d/1nS1aZpwnZbrM6BdTjoku7OvE7HhMccg9ax08x9jqh6o/edit#heading=h.59ugglgvejhs
---
+++ migrations/1667385788-boostrap-migration.js
@@ -0,0 +1,63 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("admins", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+    await queryInterface.addIndex("admins", ["email"]);
+    await queryInterface.createTable("resetPasswordTokens", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+    await queryInterface.addIndex("resetPasswordTokens", ["email"]);
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable("resetPasswordTokens");
+    await queryInterface.dropTable("admins");
+  },
+};
